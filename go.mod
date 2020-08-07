195e6e8291221d839485fba7f9420ebb94881551
bruteforce1414 <bruteforce1414@gmail.com>
2020-08-07 12:03:48 +0500

    feat: sqlx implemented

---
+++ go.mod
@@ -0,0 +1,11 @@
+module github.com/nori-io/sql-sqlx
+
+go 1.14
+
+require (
+	github.com/Masterminds/semver/v3 v3.1.0 // indirect
+	github.com/jinzhu/gorm v1.9.15
+	github.com/jmoiron/sqlx v1.2.0
+	github.com/nori-io/common/v3 v3.0.0
+	github.com/nori-io/interfaces/public/sql/sqlx v0.0.0-20200807053739-297a5539ac37
+)
---
+++ sqlx.go
@@ -0,0 +1,99 @@
+
+package main
+
+import (
+"context"
+
+"github.com/jmoiron/sqlx"
+"github.com/nori-io/common/v3/config"
+"github.com/nori-io/common/v3/logger"
+"github.com/nori-io/common/v3/meta"
+"github.com/nori-io/common/v3/plugin"
+i "github.com/nori-io/interfaces/public/sql/sqlx"
+
+_ "github.com/jinzhu/gorm/dialects/mssql"
+_ "github.com/jinzhu/gorm/dialects/mysql"
+_ "github.com/jinzhu/gorm/dialects/postgres"
+_ "github.com/jinzhu/gorm/dialects/sqlite"
+)
+
+type service struct {
+	db     *sqlx.DB
+	config *pluginConfig
+	logger logger.FieldLogger
+}
+
+type pluginConfig struct {
+	dsn     string
+	dialect string
+	logMode bool
+}
+
+var (
+	Plugin plugin.Plugin = &service{}
+)
+
+func (p *service) Init(ctx context.Context, config config.Config, log logger.FieldLogger) error {
+	p.logger = log
+	p.config.logMode = config.Bool("logMode", "log mode: true or false")()
+	p.config.dsn = config.String("dsn", "database connection string")()
+	p.config.dialect = config.String("dialect", "sql dialect: mssql, mysql, postgres, sqlite")()
+	return nil
+}
+
+func (p *service) Instance() interface{} {
+	return p.db
+}
+
+func (p *service) Meta() meta.Meta {
+	return &meta.Data{
+		ID: meta.ID{
+			ID:      "sql/sqlx",
+			Version: "1.2.0",
+		},
+		Author: meta.Author{
+			Name: "Nori.io",
+			URI:  "https://nori.io/",
+		},
+		Core: meta.Core{
+			VersionConstraint: "=0.2.0",
+		},
+		Dependencies: []meta.Dependency{},
+		Description: meta.Description{
+			Name:        "Nori: Sqlx",
+			Description: "This plugin implements instance of Sqlx",
+		},
+		Interface: i.SqlxInterface,
+		License: []meta.License{
+			{
+				Title: "GPLv3",
+				Type:  "GPLv3",
+				URI:   "https://www.gnu.org/licenses/"},
+		},
+		Tags: []string{"sqlx", "sql", "database", "db"},
+	}
+}
+
+func (p *service) Start(ctx context.Context, registry plugin.Registry) error {
+	var err error
+	p.db, err = sqlx.Open(p.config.dialect, p.config.dsn)
+	if err != nil {
+		p.logger.Error(err.Error())
+	}
+	/*	else {
+	p.db.LogMode(p.config.logMode)
+		p.db.SetLogger(&hook.Logger{Origin: p.logger})
+	}
+	*/
+	return err
+}
+
+func (p *service) Stop(ctx context.Context, registry plugin.Registry) error {
+	err := p.db.Close()
+	if err != nil {
+		p.logger.Error(err.Error())
+	}
+
+	return err
+}
+
